4f355f66b3237378fb00b84fd60a521475efc10f
Vivek Awasthi <vivekawasthi055@gmail.com>
2024-03-30 13:27:51 +0530

    new pic added

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -10,7 +10,7 @@ const Info = () => {
     <>
       <div className="main-container">
         <div className="img-box">
-          <img src="../../../images/vivek.jpg" alt="Vivek" className="my-img" />
+          <img src="../../../images/vivek.png" alt="Vivek" className="my-img" />
         </div>
         <div className="text-box">
           <div className="h4-box">
